4d31d84a0a9a59b6a0efe9ea650e0c7bf043476e
sean.bai <sean.bai@silksoftware.com>
2020-07-02 16:58:26 +0800

    master

---
+++ common/config/main.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ua',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => \yii\swiftmailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => Swift_SmtpTransport::class,
+                'host' => 'smtp.qiye.163.com',
+                'port' => 465,
+                'encryption' => 'ssl',
+                'username' => 'No-reply@underarmour.cn',
+                'password' => 'ua12345$',
+            ],
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
