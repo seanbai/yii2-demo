45480d01587ed70571a6a0475d3f3e10efad52b6
sean.bai <sean.bai@silksoftware.com>
2020-07-02 17:13:00 +0800

    初始化

--- common/config/main.php
+++ common/config/main.php
@@ -16,21 +16,5 @@ return [
             'password' => '',
             'charset' => 'utf8',
         ],
-        'mailer' => [
-            'class' => \yii\swiftmailer\Mailer::class,
-            'viewPath' => '@common/mail',
-            'transport' => [
-                'class' => Swift_SmtpTransport::class,
-                'host' => 'smtp.qiye.163.com',
-                'port' => 465,
-                'encryption' => 'ssl',
-                'username' => 'No-reply@underarmour.cn',
-                'password' => 'ua12345$',
-            ],
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
     ],
 ];
